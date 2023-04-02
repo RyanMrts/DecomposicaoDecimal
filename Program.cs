b05c28ebd5af75cbaa919205d8c60b2780dd5c10
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-02 20:59:05 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int n1, unidade, dezena, centena, Nalterado;
+
+Console.WriteLine("Digite um numero inteiro: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Nalterado=n1;
+
+unidade = Nalterado % 10;
+Nalterado /= 10;
+
+dezena = Nalterado % 10;
+Nalterado/= 10;
+
+centena = Nalterado;
+
+Console.WriteLine($"O numero {n1} possui: ");
+Console.WriteLine($"{unidade} unidade(s) ");
+Console.WriteLine($"{dezena} dezena(s)");
+Console.WriteLine($"{centena} centena(s)");
